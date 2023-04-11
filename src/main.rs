89180d7493fc0fa41876e503b7ae46dd47c63d4b
Ray Redondo <raydredondo@gmail.com>
2023-04-10 23:22:15 -0500

    cargo fmt (no release)

--- src/main.rs
+++ src/main.rs
@@ -406,48 +406,50 @@ impl eframe::App for NotesApp {
                 x.consume_key(Modifiers::CTRL, egui::Key::Enter)
                     || x.consume_key(Modifiers::SHIFT, egui::Key::Enter)
             });
-            egui::ScrollArea::vertical().show(ui, |ui| ui.add_sized(ui.available_size(), move |ui: &mut Ui| {
-                let text_edit = egui::TextEdit::multiline(&mut self.notes_text);
-                let mut output = text_edit.show(ui);
-                if eval {
-                    if let Some(cursor) = output.cursor_range {
-                        let p_idx = cursor.primary.ccursor.index;
-                        let s_idx = cursor.secondary.ccursor.index;
-                        let start = if p_idx == s_idx {
-                            self.notes_text[..p_idx]
-                                .rfind(|x| x == '=' || x == '\n')
-                                .map_or(0, |x| x + 1)
-                        } else {
-                            p_idx.min(s_idx)
-                        };
-                        let end = p_idx.max(s_idx);
-                        let text = &self.notes_text[start..end];
-                        let result = evaluate(text);
-                        let insertion = format!(
-                            " = {}",
-                            match result {
-                                Ok(x) => x.to_string(),
-                                Err(x) => x.to_string(),
-                            }
-                        );
-                        output.state.set_ccursor_range(Some(
-                            egui::widgets::text_edit::CCursorRange {
-                                primary: egui::text::CCursor {
-                                    index: end + insertion.len(),
-                                    prefer_next_row: true,
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.add_sized(ui.available_size(), move |ui: &mut Ui| {
+                    let text_edit = egui::TextEdit::multiline(&mut self.notes_text);
+                    let mut output = text_edit.show(ui);
+                    if eval {
+                        if let Some(cursor) = output.cursor_range {
+                            let p_idx = cursor.primary.ccursor.index;
+                            let s_idx = cursor.secondary.ccursor.index;
+                            let start = if p_idx == s_idx {
+                                self.notes_text[..p_idx]
+                                    .rfind(|x| x == '=' || x == '\n')
+                                    .map_or(0, |x| x + 1)
+                            } else {
+                                p_idx.min(s_idx)
+                            };
+                            let end = p_idx.max(s_idx);
+                            let text = &self.notes_text[start..end];
+                            let result = evaluate(text);
+                            let insertion = format!(
+                                " = {}",
+                                match result {
+                                    Ok(x) => x.to_string(),
+                                    Err(x) => x.to_string(),
+                                }
+                            );
+                            output.state.set_ccursor_range(Some(
+                                egui::widgets::text_edit::CCursorRange {
+                                    primary: egui::text::CCursor {
+                                        index: end + insertion.len(),
+                                        prefer_next_row: true,
+                                    },
+                                    secondary: egui::text::CCursor {
+                                        index: end + insertion.len(),
+                                        prefer_next_row: true,
+                                    },
                                 },
-                                secondary: egui::text::CCursor {
-                                    index: end + insertion.len(),
-                                    prefer_next_row: true,
-                                },
-                            },
-                        ));
-                        output.state.store(ctx, output.response.id);
-                        self.notes_text.insert_str(end, &insertion);
+                            ));
+                            output.state.store(ctx, output.response.id);
+                            self.notes_text.insert_str(end, &insertion);
+                        }
                     }
-                }
-                output.response
-            }));
+                    output.response
+                })
+            });
         });
     }
 
